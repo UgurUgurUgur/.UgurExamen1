e4644c4c473701bb857106270e364891ecd574b5
UgurUgurUgur <ugurkankaraca03@gmail.com>
2023-01-31 16:30:03 +0100

    examen

---
+++ Ugur_Examen_LoginApp/Ugur_Examen_LoginApp/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.WachtwoordTB = New System.Windows.Forms.TextBox()
+        Me.WarningMsg = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(12, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(776, 52)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Login"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(326, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 26)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "voornaam"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(375, 117)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(45, 34)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "ok"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox2.Location = New System.Drawing.Point(326, 85)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(150, 26)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "achternaam"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(375, 117)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(45, 34)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "ok"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(12, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(776, 29)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(342, 157)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(112, 34)
+        Me.LoginButton.TabIndex = 6
+        Me.LoginButton.Text = "Login"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'WachtwoordTB
+        '
+        Me.WachtwoordTB.Location = New System.Drawing.Point(326, 120)
+        Me.WachtwoordTB.Name = "WachtwoordTB"
+        Me.WachtwoordTB.Size = New System.Drawing.Size(150, 31)
+        Me.WachtwoordTB.TabIndex = 7
+        Me.WachtwoordTB.Text = "wachtwoord"
+        Me.WachtwoordTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'WarningMsg
+        '
+        Me.WarningMsg.AutoSize = True
+        Me.WarningMsg.Location = New System.Drawing.Point(248, 194)
+        Me.WarningMsg.Name = "WarningMsg"
+        Me.WarningMsg.Size = New System.Drawing.Size(297, 125)
+        Me.WarningMsg.TabIndex = 8
+        Me.WarningMsg.Text = "*Wachtwoord benodigdheden:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "min. 8 karakters lang" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "min. 1 cijfer bevatten" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "min. 1" &
+    " speciaal symbool " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "[""~`!@#$%^&*()-_=+';:,./<>] bevatten"
+        Me.WarningMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(482, 122)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(179, 29)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "toon wachtwoord"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(713, 37)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 34)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.WarningMsg)
+        Me.Controls.Add(Me.WachtwoordTB)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LoginButton As Button
+    Friend WithEvents WachtwoordTB As TextBox
+    Friend WithEvents WarningMsg As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Ugur_Examen_LoginApp/Ugur_Examen_LoginApp/Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+    Public minLenghtPassword As Integer = 8
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.Hide()
+        Button2.Hide()
+        Label2.Hide()
+        WarningMsg.ForeColor() = Color.Red
+        WarningMsg.Hide()
+        WachtwoordTB.Hide()
+        LoginButton.Hide()
+        CheckBox1.Hide()
+
+    End Sub
+
+    Private Sub VoornaamTB_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub AchternaamTB_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub FirstOK_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Hide()
+        Button1.Hide()
+        TextBox2.Show()
+        Button2.Show()
+    End Sub
+
+    Private Sub SecondOK_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "Welkom"
+        Label2.Text = TextBox1.Text & " " & TextBox2.Text
+        Button2.Hide()
+        TextBox2.Hide()
+        Label2.Show()
+        WachtwoordTB.Show()
+        LoginButton.Show()
+        CheckBox1.Show()
+    End Sub
+
+    Private Sub Username_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub WachtwoordTB_TextChanged(sender As Object, e As EventArgs) Handles WachtwoordTB.TextChanged
+
+    End Sub
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        'Dim specialChar As Boolean
+        'If InStr(1, "~`!@#$%^&*()-_=+';:,./<>", Me.WachtwoordTB.Text) > 0 Then
+        '    specialChar = True
+        'End If
+        Dim number As New System.Text.RegularExpressions.Regex("[0-9]")
+        Dim alphabet As New System.Text.RegularExpressions.Regex("[A-Z],[a-z]")
+        'Dim specialChar As New System.Text.RegularExpressions.Regex("~`!@#\$%\^&\*\(\)-_=\+;:,\./<>")
+        'If WachtwoordTB.Text.Length < minLenghtPassword Then
+        '    MsgBox("lengte 8 char")
+
+        'ElseIf number.Matches(WachtwoordTB.Text).Count < 1 Then
+        '    MsgBox("numbers")
+
+        'ElseIf specialChar.Matches(WachtwoordTB.Text).Count < 1 Then
+        '    MsgBox("specioals")
+        'Else
+        '    MsgBox("Succes!")
+        'End If
+
+        If WachtwoordTB.Text.Length < minLenghtPassword Or number.Matches(WachtwoordTB.Text).Count < 1 Then
+            WarningMsg.Show()
+        Else
+            MsgBox("succes!")
+        End If
+    End Sub
+
+    Private Sub WarningMsg_Click(sender As Object, e As EventArgs) Handles WarningMsg.Click
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        'CHECKING IF THE CHECKBOX WAS CHECKED OR NOT.
+        If CheckBox1.CheckState = CheckState.Checked Then
+            'IF TRUE, IT SHOW THE TEXT
+            WachtwoordTB.UseSystemPasswordChar = False
+        Else
+            'IF FALSE, IT WILL HIDE THE TEXT AND IT WILL TURN IT INTIO BULLETS.
+            WachtwoordTB.UseSystemPasswordChar = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text = "Login"
+        TextBox1.Text = "voornaam"
+        TextBox1.Show()
+        TextBox2.Text = "achternaam"
+        Button1.Show()
+        Button2.Hide()
+        Label2.Hide()
+        WarningMsg.Hide()
+        WachtwoordTB.Hide()
+        WachtwoordTB.Text = "wachtwoord"
+        LoginButton.Hide()
+        CheckBox1.Hide()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
